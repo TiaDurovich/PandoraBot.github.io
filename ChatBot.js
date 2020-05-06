96fba797660c775d41d6508de7dce724e0954f3f
Tia Durovich <tiadurovich@Tias-MacBook-Pro.local>
2020-05-06 13:09:41 +1200

    Interpreting Multiple 'Yes' Responses using a range of 'If' statements 2

--- ChatBot.js
+++ ChatBot.js
@@ -91,64 +91,64 @@ function response() {
         user.textContent = userInput.value;
     }
     if(userInput.value == "Yes") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "yes") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "Yeah") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "yeah") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "Ya") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "ya") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "Y") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "y") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "Yes I did") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "Yes, I did") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "yes i did") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "Yes i did") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "Yup") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "yup") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "Sure did") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "sure did") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "Yas") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "yas") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "Yo") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "yo") { 
-        botChat.textContent = didYouKnowResponseYes[randomYesResponse]
+        botChat.textContent = didYouKnowResponseYes[randomNumber]
     }
     if(userInput.value == "No") {
         botChat.textContent = didYouKnowResponseNo[randomNumber]
