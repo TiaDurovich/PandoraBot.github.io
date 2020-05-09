5f1147722cc31a4be9b2b44c140517d4c554de10
Tia Durovich <tiadurovich@Tias-MacBook-Pro.local>
2020-05-09 14:08:49 +1200

    Stop Follow Question From Appearing Too Quickly Test 1

--- ChatBot.js
+++ ChatBot.js
@@ -68,7 +68,7 @@ function response() {
         user.textContent = userInput.value;
     }
     if (userInput.value == "Yes") {
-        botChat.textContent = didYouKnowResponseYes[randomNumber]
+        botChat.textContent = didYouKnowResponseYes[randomNumber];
         setTimeout(() => {botChat.textContent = "Want to know about celestial bodies you can see in the night sky?"}, 2500)
     }
     if(userInput.value == "yes") { 
@@ -204,7 +204,7 @@ function response() {
         setTimeout(() => {botChat.textContent = "Want to know about celestial bodies you can see in the night sky?"}, 2500)
     }
 
-    
+
     if (botChat.textContent == "Want to know about celestial bodies you can see in the night sky?") {
         while (userInput.value == "Yes") {
             botChat.textContent = "Awesome! I need to know what country you are in... or even better, tell me what hemisphere you are located within! North or South?";
