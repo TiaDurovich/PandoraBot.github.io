10452c9e26de73ac508fd7827971d992c23c0267
Tia Durovich <tiadurovich@Tias-MacBook-Pro.local>
2020-05-09 13:57:48 +1200

    Making didYouKnowResponseYes Run Only Once Test 12

--- ChatBot.js
+++ ChatBot.js
@@ -204,10 +204,10 @@ function response() {
     }
 
     if (botChat.textContent == "Want to know about celestial bodies you can see in the night sky?") {
-        while (userInput.value == "Yes") {
-            botChat.textContent = "Awesome! I need to know what country you are in... or even better, tell me what hemisphere you are located within! North or South?";
+            botChat.textContent = "Awesome! I need to know what country you are in... or even better, tell me what hemisphere you are located within! North or South?" {
+                while userInput.textContent = "Yes"
+            }
         }
-    }
 }
 
 
